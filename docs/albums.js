63078bcfa307282d330fdb9d6c86b54143552ed3
ian6868 <thiscrazylife@live.com>
2024-05-04 08:53:46 -0400

    Update albums.js
    updated first two tracks on each side

--- docs/albums.js
+++ docs/albums.js
@@ -18,33 +18,33 @@
 
 const albums = [
     {
-        name: ' ',
-        year: 2023,
-        images: ['./1.jpg'],
-        backgrounds: ['./2.jpg', './2.jpg'],
-        track_gradient_start: '#324f75',
-        track_gradient_end: '#383b5e',
-        track_hover_color: '#2349A2',
+        name: 'I Probably Should Not Have Said That',
+        year: 2024,
+        images: ['./ALBUM.png', './2.png', './1.png'],
+        backgrounds: ['./bg.png', './bg.png'],
+        track_gradient_start: '#ff3000',
+        track_gradient_end: '#fee400',
+        track_hover_color: '#0000F6',
         tracks_front: [
             {
-                name: 'Angelic Anguish',
-                path: "./angelicanguish.mp3",
-                image: './1.gif',
+                name: 'A Marie',
+                path: './AMarie.ogg',
+                image: './AMarie.png',
             }, {
-                name: 'Catching Z\'s',
-                path: "./catchingzs.mp3",
-                image: './2.gif',
+                name: 'Allo',
+                path: './Allo.ogg',
+                image: './Allo2.png',
             }
         ],
         tracks_back: [
             {
-                name: 'Halcyon',
-                path: "./halcyon.mp3",
-                image: './3.gif',
+                name: 'Midnight',
+                path: './Midnight.ogg',
+                image: './mIDNIGHT2.png',
             }, {
-                name: 'Too Scared of the Future',
-                path: "./tooscared.mp3",
-                image: './4.gif',
+                name: 'Standing Undressed',
+                path: './StandingUndressed.ogg',
+                image: './StandingUndressed2.png',
             },
         ]
 
